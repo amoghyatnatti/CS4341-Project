dcf5be353197af9054c7c73183709e25bbbb10de
amoghyatnatti <amoghyatnatti@gmail.com>
2021-05-11 14:11:56 -0500

    Finished 4341 Project

--- alu.v
+++ alu.v
@@ -1,5 +1,9 @@
-//DFF Module
-
+/*
+ * ALU w/ 32-bit Accumulator Register
+ * Cohort Name: Logical Cohort
+ * Cohort Members: Amogh Yatnatti, Vineet Goyal, Sanika Buche, Muhammad Khan, Unnati Patel
+ * 
+ */
 
 //OpCodes
 `define NOOP  '4b0000 //00=system, 00=NOOP
@@ -14,228 +18,196 @@
 `define NOT   '4b1010 //10=Logic, 10=NOT
 `define XOR   '4b1011 //10=Logic, 11=XOR
 
-//FLip Flop
+// 16 bit AND
+module and_gate(inputA, inputB, outputC);
+    input [15:0] inputA;
+    input [15:0] inputB;
+	wire [15:0] inputA;
+    wire [15:0] inputB;
+    output[31:0] outputC;
+    assign outputC = inputA & inputB;
+endmodule
+
+// 16 bit NOT
+module not_gate(inputB, outputC);
+    input [15:0] inputB;
+	wire [15:0] inputB;
+    output [31:0] outputC;
+    assign outputC =~ inputB;
+endmodule
+
+// 16 bit XOR
+module xor_gate(inputA, inputB, outputC);
+    input [15:0] inputA;
+    input [15:0] inputB;
+    wire [15:0] inputA;
+    wire [15:0] inputB;
+    output [31:0] outputC;
+    assign outputC = inputA ^ inputB;
+endmodule
+
+// 16 bit OR
+module or_gate(inputA, inputB, outputC);
+    input [15:0] inputA;
+    input [15:0] inputB;
+    wire [15:0] inputA;
+    wire [15:0] inputB;
+    output [31:0] outputC;
+    assign outputC = inputA | inputB;
+endmodule
+
+// 16 bit ADDITION
+module addition(inputA, inputB, outputC);
+    input [15:0] inputA;
+    input [15:0] inputB;
+    wire [15:0] inputA;
+    wire [15:0] inputB;
+    output [31:0] outputC;
+    assign outputC = inputA + inputB;
+endmodule
+
+// 16 bit SUBTRACTION
+module subtraction(inputA, inputB, outputC);
+    input [15:0] inputA;
+    input [15:0] inputB;
+    wire [15:0] inputA;
+    wire [15:0] inputB;
+    output [31:0] outputC;
+    assign outputC = inputA - inputB;
+endmodule
+
+// 16 bit MULTIPLICATION
+module multiplication(inputA, inputB, outputC);
+    output [31:0] outputC;
+	input  [15:0] inputA;
+	input  [15:0] inputB;
+	assign outputC = inputA * inputB;
+endmodule
+
+// 16 bit DIVISION
+module division(inputA, inputB, outputC);
+    output [31:0] outputC;
+	input  [15:0] inputA;
+	input  [15:0] inputB;
+	assign outputC = inputA / inputB;
+endmodule
+
+//MUX Multiplexer 16 by 32
+module MUX(channels,select,b);
+    input [15:0][31:0]channels;
+    input [3:0] select;
+    output [31:0] b;
+    wire[15:0][31:0] channels;
+    reg [31:0] b;
+    always @(*)
+    begin
+        b = channels[select];
+    end
+endmodule
+
+// D-Flip Flop
 module DFF(clk,in,out);
-	parameter n=1;//width
-	input clk;
-	input [n-1:0] in;
-	output [n-1:0] out;
-	reg [n-1:0] out;
-
-	always @(posedge clk)
-	out = in;
+    parameter n=1; // width
+    input clk;
+    input [n-1:0] in;
+    output [n-1:0] out;
+    reg [n-1:0] out;
+    always @(posedge clk) // positive edge
+    out = in;
 endmodule
-
-//HALF-ADDER
-module Add_half (input a, b,  output c_out, sum);
-   xor G1(sum, a, b);	 
-   and G2(c_out, a, b);
-endmodule
-
-//FULL-ADDER
-module Add_full (input a, b, c_in, output c_out, sum);
-   wire w1, w2, w3;	 
-   Add_half M1 (a, b, w1, w2);
-   Add_half M0 (w2, c_in, w3, sum);
-   or (c_out, w1, w3);
-endmodule
-
-
-// //Decoder (n to M)
-// module Dec(a,b);
-
-// parameter n=4;
-// parameter m=16;
-// input [n-1:0] a;
-// output [m-1:0] b;
-
-// assign  b= 1<<a; //Shift 1 a places. Makes a 1-hot.
-
-// endmodule
-
-//Enc 4 to 2
-module Enc42(a,b);
-input [3:0] a;
-output [1:0] b;
-assign b={a[3]|a[2],a[3]|a[1]};
-endmodule
-
-//Enc 4 to 2
-module Enc42a(a,b,c);
-input [3:0] a;
-output [1:0] b;
-output c;
-assign b={a[3]|a[2],a[3]|a[1]};
-assign c=|a;
-endmodule
-
-//Enc 16 to 4
-module Enc164(a,b);
-input [15:0] a;
-output [3:0] b;
-wire[7:0] c;
-wire[3:0] d;
-
-Enc42a e0(a[ 3: 0],c[1:0],d[0]);
-Enc42a e1(a[ 7: 4],c[3:2],d[1]);
-Enc42a e2(a[11: 8],c[5:4],d[2]);
-Enc42a e3(a[15:12],c[7:6],d[3]);
-
-Enc42 e4(d[3:0],b[3:2]);
-
-endmodule
-
-
-//MUX Multiplexer 16 by 2
-module Mux(channels,select,b);
-input [15:0][1:0]channels;
-input [3:0] select;
-output [1:0] b;
-wire[15:0][1:0] channels;
-reg [1:0] b;
-always @(*)
-begin
- b=channels[select]; 
-end
-
-endmodule
-
-
-//ADD operation
-module ADDER(inputA,inputB,outputC,carry);
-//---------------------------------------
-input [1:0] inputA;
-input [1:0] inputB;
-wire [1:0] inputA;
-wire [1:0] inputB;
-//---------------------------------------
-output [1:0] outputC;
-output carry;
-reg [1:0] outputC;
-reg carry;
-//---------------------------------------
-
-wire [1:0] S;
-wire [1:0] Cin;
-wire [1:0] Cout;
-//Link the wires between the Adders
-assign Cin[0]=0;
-assign Cin[1]=Cout[0];
-
-//Declare and Allocate 4 Full adders
-Add_full FA[1:0] (inputA,inputB,Cin,Cout,S);
-
-always @(*)
-begin
- carry=Cout[1];
- outputC=S;
-end
-
-endmodule
-
-//AND operation
-module ANDER(inputA,inputB,outputC);
-input [1:0] inputA;
-input [1:0] inputB;
-output [1:0] outputC;
-wire [1:0] inputA;
-wire [1:0] inputB;
-reg [1:0] outputC;
-
-reg [1:0] result;
-
-always@(*)
-begin
-	result[0]=inputA[0]&inputB[0];
-	result[1]=inputA[1]&inputB[1];
-	outputC=result;
-end
- 
-endmodule
-
-//Accumulator Register
-
 
 //Breadboard
 module breadboard(clk,rst,A,B,C,opcode);
-//----------------------------------
-input clk; 
-input rst;
-input [1:0] A;
-input [1:0] B;
-input [3:0] opcode;
-wire clk;
-wire rst;
-wire [1:0] A;
-wire [1:0] B;
-wire [3:0] opcode;
-//----------------------------------
-output [1:0] C;
-reg [1:0] C;
-//----------------------------------
+    input clk; 
+    input rst;
+    input [15:0] A;
+    input [15:0] B;
+    input [3:0] opcode;
+    wire clk;
+    wire rst;
+    wire [15:0] A;
+    wire [15:0] B;
+    wire [3:0] opcode;
 
-wire [15:0][1:0]channels;
-wire [1:0] b;
-wire [1:0] outputADD;
-wire [1:0] outputAND;
+    output reg [31:0] C;
+    wire [15:0][31:0]channels;
+    wire [31:0] b;
+    
+    // Channel outputs
+    wire [31:0] outputADD;
+    wire [31:0] outputAND;
+    wire [31:0] outputXOR;
+    wire [31:0] outputOR;
+    wire [31:0] outputNOT;
+    wire [31:0] outputSUB;
+    wire [31:0] outputPRESET;
+    wire [31:0] outputMULT;
+    wire [31:0] outputDIV;
+    assign outputPRESET = {32{1'b1}};
+    
+    reg [15:0] regA;
+    reg [15:0] regB;
+    reg  [31:0] next;
+    wire [31:0] cur;
 
-reg [1:0] regA;
-reg [1:0] regB;
+    // Multiplexor
+    MUX mux1(channels,opcode,b);
 
-reg  [1:0] next;
-wire [1:0] cur;
+    // Logic
+    and_gate and1(regA, regB, outputAND);
+    xor_gate xor1(regA, regB, outputXOR);
+    or_gate or1(regA, regB, outputOR);
+    not_gate not1(regB, outputNOT);
 
-Mux mux1(channels,opcode,b);
-ADDER add1(regA,regB,outputADD,carry);
-ANDER and1(regA,regB,outputAND);
+    // Mathematics
+    addition add1(regA, regB, outputADD);
+    subtraction sub1(regB, regA, outputSUB);
+    multiplication mult1(regA, regB, outputMULT);
+    division div1(regB, regA, outputDIV);
 
-DFF ACC1 [1:0] (clk,next,cur);
+    // DFF for Accumulator
+    DFF ACC1 [31:0] (clk,next,cur);
 
+    assign channels[0]=cur;//NO-OP
+    assign channels[1]=0;//RESET
+    assign channels[2]=0;//GROUND=0
+    assign channels[3]=outputPRESET;//PRESET
+    assign channels[4]=outputMULT;//MULT
+    assign channels[5]=outputADD; //ADD
+    assign channels[6]=outputSUB;//SUB
+    assign channels[7]=outputDIV;//DIV
+    assign channels[8]=outputOR;//OR
+    assign channels[9]=outputAND; //AND
+    assign channels[10]=outputNOT;//NOT
+    assign channels[11]=outputXOR;//XOR
+    assign channels[12]=0;//GROUND=0
+    assign channels[13]=0;//GROUND=0
+    assign channels[14]=0;//GROUND=0
+    assign channels[15]=0;//GROUND=0
 
-assign channels[0]=cur;//NO-OP
-assign channels[1]=0;//RESET
-assign channels[2]=0;//GROUND=0
-assign channels[3]=0;//GROUND=0
-assign channels[4]=0;//GROUND=0
-assign channels[5]=outputADD;
-assign channels[6]=0;//GROUND=0
-assign channels[7]=0;//GROUND=0
-assign channels[8]=0;//GROUND=0
-assign channels[9]=outputAND;
-assign channels[10]=0;//GROUND=0
-assign channels[11]=0;//GROUND=0
-assign channels[12]=0;//GROUND=0
-assign channels[13]=0;//GROUND=0
-assign channels[14]=0;//GROUND=0
-assign channels[15]=0;//GROUND=0
+    always @(*)
+    begin
+    regA=A;
+    regB=cur;
 
-always @(*)
-begin
- regA=A;
- regB=cur;
-
- assign C=b;
- assign next=b;
-end
-
+    assign C=b;
+    assign next=b;
+    end
 endmodule
-
 
 //TEST BENCH
 module testbench();
 
-//Local Variables
+   //Local Variables
    reg clk;
    reg rst;
-   reg [1:0] inputA;
-   reg [1:0] inputB;
-   wire [1:0] outputC;
+   reg [15:0] inputA;
+   reg [15:0] inputB;
+   wire [31:0] outputC;
    reg [3:0] opcode;
-   reg [15:0] count;
 
-// create breadboard
-breadboard bb8(clk,rst,inputA,inputB,outputC,opcode);
-
+    // create breadboard
+    breadboard bb8(clk,rst,inputA,inputB,outputC,opcode);
 
    //CLOCK
    initial begin //Start Clock Thread
@@ -248,112 +220,82 @@ breadboard bb8(clk,rst,inputA,inputB,outputC,opcode);
         end
     end
 
-
-
     initial begin //Start Output Thread
 	forever
          begin
-		 $display("(ACC:%2b)(OPCODE:%4b)(IN:%2b)(OUT:%2b)",bb8.cur,opcode,inputA,bb8.b);
+		 $display("(ACC:%32b)(OPCODE:%4b)(IN:%16b)(OUT:%32b)",bb8.cur,opcode,inputA,bb8.b);
 		 #10;
 		 end
 	end
 
-//STIMULOUS
+    //STIMULOUS
 	initial begin//Start Stimulous Thread
-	#6;	
-	//---------------------------------
-	inputA=2'b00;
+	#6;
+    //---------------------------------
+	inputA=16'b0000000000000000;
 	opcode=4'b0000;//NO-OP
 	#10; 
 	//---------------------------------
-	inputA=2'b00;
+	inputA=16'b0000000000000000;
 	opcode=4'b0001;//RESET
 	#10
 	//---------------------------------	
-	inputA=2'b00;
-	opcode=4'b0000;//NO OP
-	#10;
-	//---------------------------------	
-	inputA=2'b00;
-	opcode=4'b0000;//NO OP
-	#10;
-	//---------------------------------	
-	inputA=2'b01;
-	opcode=4'b0101;//ADD
-	#10;
-	//---------------------------------
-	inputA=2'b00;
-	opcode=4'b0000;//NOOP
-	#10;
-	//---------------------------------	
-	inputA=2'b01;
-	opcode=4'b0101;//ADD
-	#10
-	//---------------------------------	
-	inputA=2'b00;
-	opcode=4'b0000;//NOOP
-	#10;
-	//---------------------------------	
-	inputA=2'b01;
-	opcode=4'b0101;//ADD
-	#10
-	//---------------------------------	
-	inputA=2'b00;
-	opcode=4'b0000;//NOOP
-	#10;
-	//---------------------------------	
-	inputA=2'b00;
-	opcode=4'b0001;//RESET
-	#10;
-	//---------------------------------	
-	inputA=2'b00;
-	opcode=4'b0000;//NOOP
-	#10;
-	//---------------------------------	
-	inputA=2'b11;
-	opcode=4'b0101;//ADD
-	#10
-	//---------------------------------	
-	inputA=2'b00;
-	opcode=4'b0000;//NOOP
-	#10;
-	//---------------------------------	
-	inputA=2'b10;
+	inputA=16'b0000000000000000;
 	opcode=4'b1001;//AND
 	#10;
 	//---------------------------------	
-	inputA=2'b00;
-	opcode=4'b0000;//NOOP
+	inputA=16'b0000000011111111;
+	opcode=4'b1010;//NOT
 	#10;
 	//---------------------------------	
-	inputA=2'b00;
-	opcode=4'b0001;//Reset
-	#10;
-	//---------------------------------	
-	inputA=2'b00;
-	opcode=4'b0000;//NOOP
-	#10;
-	//---------------------------------	
-	inputA=2'b01;
+	inputA=16'b0000000000001111;
 	opcode=4'b0101;//ADD
-	
-	//Uh-oh...it was left in the ADD operation...its an addtion STATE!
-	#10
-	#10
-	#10
-	#10
-	#10
-	#10
-	//---------------------------------	
-	inputA=2'b00;
+	#10;
+    //---------------------------------	
+	inputA=16'b0000000000000000;
+	opcode=4'b1011;//XOR
+	#10;
+    //---------------------------------	
+	inputA=16'b0000000000001111;
+	opcode=4'b0110;//SUB
+	#10;
+    //---------------------------------	
+    inputA=16'b0000000000000000;
 	opcode=4'b0001;//RESET
 	#10;
-	//---------------------------------	
-	inputA=2'b00;
-	opcode=4'b0000;//NOOP
-	#10
-	
+    //---------------------------------	
+	inputA=16'b0000000000000000;
+	opcode=4'b0011;//PRESET
+	#10;
+    //---------------------------------	
+	inputA=16'b0000000000000000;
+	opcode=4'b1000;//OR
+	#10;
+    //---------------------------------	
+	inputA=16'b0000000000000000;
+	opcode=4'b0000;//NO OP
+	#10;
+    //---------------------------------	
+	inputA=16'b0000000000001111;
+	opcode=4'b0100;//MULT
+	#10;
+    //---------------------------------	
+	inputA=16'b0000000000000010;
+	opcode=4'b0111;//DIV
+	#10;
+    //---------------------------------	
+	inputA=16'b0000000000000000;
+	opcode=4'b0011;//PRESET
+	#10;
+    //---------------------------------	
+	inputA=16'b0000000000000001;
+	opcode=4'b0101;//ADD
+	#10;
+
+    #10; 
+    #10;
+    #10; // CONTINUES ADD OPERATION
+    //---------------------------------	
 	$finish;
 	end
-
 endmodule
